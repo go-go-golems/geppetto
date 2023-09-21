81cbe61c7f467a3b5fbe5fec76bd27bd511b6aed
Manuel Odendahl <wesen@ruinwesen.com>
2023-09-21 16:27:16 -0500

    :art: Properly handle scrolling of the previous messages when new message is output

--- cmd/chat/style.go
+++ cmd/chat/style.go
@@ -29,19 +29,19 @@ func DefaultStyles() *Style {
 
 	return &Style{
 		UnselectedMessage: lipgloss.NewStyle().Border(lipgloss.NormalBorder()).
-			Padding(1, 1).
+			Padding(0, 1).
 			BorderForeground(lipgloss.AdaptiveColor{
 				Light: lightModeColors.Unselected,
 				Dark:  darkModeColors.Unselected,
 			}),
 		SelectedMessage: lipgloss.NewStyle().Border(lipgloss.ThickBorder()).
-			Padding(1, 1).
+			Padding(0, 1).
 			BorderForeground(lipgloss.AdaptiveColor{
 				Light: lightModeColors.Selected,
 				Dark:  darkModeColors.Selected,
 			}),
 		FocusedMessage: lipgloss.NewStyle().Border(lipgloss.NormalBorder()).
-			Padding(1, 1).
+			Padding(0, 1).
 			BorderForeground(lipgloss.AdaptiveColor{
 				Light: lightModeColors.Focused,
 				Dark:  darkModeColors.Focused,
