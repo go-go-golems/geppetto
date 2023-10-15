46f0172885e5f22ba69cd8297db44cadaeb2553b
Manuel Odendahl <wesen@ruinwesen.com>
2023-10-15 07:27:04 -0400

    :fire: :ambulance: Remove deprecated commands, improve loading of API key

--- cmd/pinocchio/cmds/openai/openai.go
+++ cmd/pinocchio/cmds/openai/openai.go
@@ -1,28 +1,20 @@
 package openai
 
 import (
-	"bytes"
 	"context"
 	_ "embed"
-	"encoding/json"
-	"fmt"
 	get_conversation "github.com/go-go-golems/geppetto/cmd/pinocchio/cmds/openai/get-conversation"
 	"github.com/go-go-golems/geppetto/pkg/steps/ai/settings/openai"
 	"github.com/go-go-golems/glazed/pkg/cli"
 	"github.com/go-go-golems/glazed/pkg/cmds"
 	"github.com/go-go-golems/glazed/pkg/cmds/layers"
 	"github.com/go-go-golems/glazed/pkg/cmds/parameters"
-	"github.com/go-go-golems/glazed/pkg/help"
 	"github.com/go-go-golems/glazed/pkg/middlewares"
 	"github.com/go-go-golems/glazed/pkg/settings"
 	"github.com/go-go-golems/glazed/pkg/types"
 	"github.com/mb0/glob"
-	"github.com/pkg/errors"
 	openai2 "github.com/sashabaranov/go-openai"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
-	"strings"
-	"text/template"
 )
 
 var OpenaiCmd = &cobra.Command{
@@ -198,200 +190,6 @@ func (c *EngineInfoCommand) Run(
 	return nil
 }
 
-//go:embed help/models.json
-var modelsJson string
-
-//go:embed help/help-family-template.md
-var helpFamilyTemplate string
-
-//go:embed help/help-completion-template.md
-var helpCompletionTemplate string
-
-type Completion struct {
-	Name                   string `json:"name"`
-	Family                 string `json:"family"`
-	Description            string `json:"description"`
-	MaxTokens              int    `json:"max_tokens"`
-	TrainingDataCutoffDate string `json:"training_data_cutoff_date"`
-}
-
-type Family struct {
-	Name             string   `json:"name"`
-	Description      string   `json:"description"`
-	PricePer1kTokens float64  `json:"price_per_1k_tokens"`
-	GoodAt           []string `json:"good_at"`
-	KeyPoints        []string `json:"key_points"`
-	Subtitle         string   `json:"subtitle"`
-	Short            string   `json:"short"`
-}
-
-type ModelsJSON struct {
-	Completion []Completion `json:"completion"`
-	Families   []Family     `json:"families"`
-}
-
-type SimpleModelsJSON struct {
-	Completion []types.Row `json:"completion"`
-	Families   []types.Row `json:"families"`
-}
-
-var FamiliesCmd = &cobra.Command{
-	Use:   "ls-families",
-	Short: "list families",
-	Run: func(cmd *cobra.Command, args []string) {
-		models := SimpleModelsJSON{}
-		err := json.Unmarshal([]byte(modelsJson), &models)
-		cobra.CheckErr(err)
-
-		ctx := cmd.Context()
-
-		gp, _, err := cli.CreateGlazedProcessorFromCobra(cmd)
-		cobra.CheckErr(err)
-
-		for _, family := range models.Families {
-			err = gp.AddRow(ctx, family)
-			cobra.CheckErr(err)
-		}
-
-		err = gp.Close(ctx)
-		cobra.CheckErr(err)
-	},
-}
-
-func NewInfoCommand() *cobra.Command {
-	return &cobra.Command{
-		Use:   "info",
-		Short: "info about openai settings",
-		Run: func(cmd *cobra.Command, args []string) {
-			openaiAPIKey := viper.GetString("openai-api-key")
-			fmt.Printf("openai-api-key: %s\n", openaiAPIKey)
-		},
-	}
-}
-
-var ModelsCmd = &cobra.Command{
-	Use:   "ls-models",
-	Short: "list models",
-	Run: func(cmd *cobra.Command, args []string) {
-		ctx := cmd.Context()
-
-		models := SimpleModelsJSON{}
-		err := json.Unmarshal([]byte(modelsJson), &models)
-		cobra.CheckErr(err)
-
-		gp, _, err := cli.CreateGlazedProcessorFromCobra(cmd)
-		cobra.CheckErr(err)
-
-		for _, completion := range models.Completion {
-			err = gp.AddRow(ctx, completion)
-			cobra.CheckErr(err)
-		}
-
-		err = gp.Close(ctx)
-		cobra.CheckErr(err)
-	},
-}
-
-func LoadModelsHelpFiles() ([]*help.Section, error) {
-	models := ModelsJSON{}
-	err := json.Unmarshal([]byte(modelsJson), &models)
-	if err != nil {
-		return nil, err
-	}
-
-	ret := []*help.Section{}
-
-	familyTemplate, err := template.New("family").Parse(helpFamilyTemplate)
-	if err != nil {
-		return nil, err
-	}
-
-	completionTemplate, err := template.New("completion").Parse(helpCompletionTemplate)
-	if err != nil {
-		return nil, err
-	}
-
-	families := map[string]*Family{}
-
-	for _, family := range models.Families {
-		families[family.Name] = &family
-
-		buf := &bytes.Buffer{}
-		err = familyTemplate.Execute(buf, family)
-		if err != nil {
-			return nil, errors.Wrapf(err, "error executing family template for family %s", family.Name)
-		}
-
-		familySection := &help.Section{
-			Slug:        strings.ToLower(family.Name),
-			SectionType: help.SectionGeneralTopic,
-			Title:       fmt.Sprintf("OpenAI Family %s", family.Name),
-			SubTitle:    family.Subtitle,
-			Short:       family.Short,
-			Content:     buf.String(),
-			Topics: []string{
-				"models",
-				"openai",
-				"families",
-			},
-			Flags: []string{
-				"engine",
-			},
-			Commands:       nil,
-			IsTopLevel:     true,
-			IsTemplate:     false,
-			ShowPerDefault: false,
-			Order:          0,
-		}
-
-		ret = append(ret, familySection)
-	}
-
-	for _, completion := range models.Completion {
-		family, ok := families[completion.Family]
-		if !ok {
-			return nil, fmt.Errorf("family %s not found", completion.Family)
-		}
-
-		data := map[string]interface{}{
-			"Completion": completion,
-			"Family":     family,
-		}
-
-		buf := &bytes.Buffer{}
-		err = completionTemplate.Execute(buf, data)
-		if err != nil {
-			return nil, errors.Wrapf(err, "error executing template for completion %s", completion.Name)
-		}
-
-		completionSection := &help.Section{
-			Slug:        strings.ToLower(completion.Name),
-			SectionType: help.SectionGeneralTopic,
-			Title:       completion.Name,
-			SubTitle:    family.Subtitle,
-			Short:       completion.Description,
-			Content:     buf.String(),
-			Topics: []string{
-				"completion",
-				"openai",
-				"models",
-			},
-			Flags: []string{
-				"engine",
-			},
-			Commands:       []string{"completion"},
-			IsTopLevel:     true,
-			IsTemplate:     false,
-			ShowPerDefault: false,
-			Order:          1,
-		}
-
-		ret = append(ret, completionSection)
-	}
-
-	return ret, nil
-}
-
 func init() {
 	listEnginesCommand, err := NewListEngineCommand()
 	cobra.CheckErr(err)
@@ -411,23 +209,9 @@ func init() {
 	cobra.CheckErr(err)
 	OpenaiCmd.AddCommand(cobraGetConversationCommand)
 
-	err = cli.AddGlazedProcessorFlagsToCobraCommand(FamiliesCmd)
-	if err != nil {
-		panic(err)
-	}
-	OpenaiCmd.AddCommand(FamiliesCmd)
-
-	err = cli.AddGlazedProcessorFlagsToCobraCommand(ModelsCmd)
-	if err != nil {
-		panic(err)
-	}
-	OpenaiCmd.AddCommand(ModelsCmd)
-
 	transcribeCommand, err := NewTranscribeCommand()
 	cobra.CheckErr(err)
 	cobraTranscribeCommand, err := cli.BuildCobraCommandFromGlazeCommand(transcribeCommand)
 	cobra.CheckErr(err)
 	OpenaiCmd.AddCommand(cobraTranscribeCommand)
-
-	OpenaiCmd.AddCommand(NewInfoCommand())
 }
