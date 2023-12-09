3eb983dd16c1482a217528018fdb14d2acde4c2e
Manuel Odendahl <wesen@ruinwesen.com>
2023-12-08 23:59:04 -0500

    :sparkles: Add first codegen steps

---
+++ pkg/steps/utils/logger.go
@@ -0,0 +1,24 @@
+package utils
+
+import (
+	"context"
+	"fmt"
+	"github.com/go-go-golems/geppetto/pkg/steps"
+)
+
+// LoggerStep is a step that logs its input to stdout.
+type LoggerStep[T any] struct{}
+
+var _ steps.Step[string, string] = &LoggerStep[string]{}
+
+// Start implements the Step interface for LoggerStep.
+// It prints the input to stdout and returns a StepResult with the same value.
+func (ls *LoggerStep[T]) Start(ctx context.Context, input T) (steps.StepResult[T], error) {
+	select {
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	default:
+		fmt.Println(input)               // Print the input to stdout
+		return steps.Resolve(input), nil // Return the input as a successful result
+	}
+}
