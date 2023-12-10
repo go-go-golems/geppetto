1c15e510824dfbc08e0d6f1d48acf8955f1f9095
Manuel Odendahl <wesen@ruinwesen.com>
2023-12-09 23:37:34 -0500

    :art: Forgot merge step

---
+++ pkg/steps/utils/merge.go
@@ -0,0 +1,31 @@
+package utils
+
+import (
+	"github.com/go-go-golems/geppetto/pkg/context"
+	"github.com/go-go-golems/geppetto/pkg/helpers"
+	"github.com/go-go-golems/geppetto/pkg/steps"
+	"time"
+)
+
+func NewMergeStep(manager *context.Manager, prepend bool) steps.Step[string, []*context.Message] {
+	var mergeStep steps.Step[string, []*context.Message]
+	mergeStep = &LambdaStep[string, []*context.Message]{
+		Function: func(input string) helpers.Result[[]*context.Message] {
+			if prepend {
+				manager.PrependMessages(&context.Message{
+					Text: input,
+					Time: time.Now(),
+					Role: context.RoleUser,
+				})
+			}
+
+			manager.AddMessages(&context.Message{
+				Text: input,
+				Time: time.Now(),
+				Role: context.RoleAssistant,
+			})
+			return helpers.NewValueResult[[]*context.Message](manager.GetMessagesWithSystemPrompt())
+		},
+	}
+	return mergeStep
+}
