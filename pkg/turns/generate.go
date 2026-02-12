8a593c30cdf1d27c947cf0b3f2dfe64dd6ec4a66
Manuel Odendahl <wesen@ruinwesen.com>
2026-02-12 09:33:54 -0500

    GP-003 Task 2: adopt generated BlockKind mapper

--- pkg/turns/generate.go
+++ pkg/turns/generate.go
@@ -1,3 +1,4 @@
 package turns
 
-//go:generate go run ../../cmd/gen-turns --schema spec/turns_codegen.yaml --out .generated --section all
+//go:generate go run ../../cmd/gen-turns --schema spec/turns_codegen.yaml --out . --section kinds
+//go:generate go run ../../cmd/gen-turns --schema spec/turns_codegen.yaml --out .generated --section keys
