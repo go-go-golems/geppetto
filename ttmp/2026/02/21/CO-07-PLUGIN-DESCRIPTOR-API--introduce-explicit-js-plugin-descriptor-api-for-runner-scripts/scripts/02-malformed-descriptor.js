7df7d6920d542d7200d03b112dca09559f9f39f0
Manuel Odendahl <wesen@ruinwesen.com>
2026-02-25 19:54:38 -0500

    hardcut: remove geppetto plugin helper leftovers

--- ttmp/2026/02/21/CO-07-PLUGIN-DESCRIPTOR-API--introduce-explicit-js-plugin-descriptor-api-for-runner-scripts/scripts/02-malformed-descriptor.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  apiVersion: "cozo.extractor/v1",
-  kind: "extractor",
-  id: "cozo.malformed",
-  name: "Malformed Descriptor",
-  // Intentionally wrong: should be a function.
-  create: {},
-};
