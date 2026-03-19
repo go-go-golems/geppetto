b854e75aebf88b3ae2d94e7628f6aa65e785f47d
Manuel Odendahl <wesen@ruinwesen.com>
2026-03-19 13:23:05 -0400

    :art: Fix gen-meta

--- pkg/inference/engine/generate.go
+++ pkg/inference/engine/generate.go
@@ -1,3 +1,3 @@
 package engine
 
-//go:generate go run ../../../cmd/gen-meta --schema ../../spec/geppetto_codegen.yaml --section engine-go
+//go:generate go run ../../../cmd/tools/gen-meta --schema ../../spec/geppetto_codegen.yaml --section engine-go
