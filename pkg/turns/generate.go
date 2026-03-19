01ed54dd6a7cb16dfe1e3223a85efb0df243f2b5
Manuel Odendahl <wesen@ruinwesen.com>
2026-03-19 13:22:34 -0400

    :art: Fix gen-meta

--- pkg/turns/generate.go
+++ pkg/turns/generate.go
@@ -1,4 +1,4 @@
 package turns
 
-//go:generate go run ../../cmd/gen-meta --schema ../spec/geppetto_codegen.yaml --section turns-go
-//go:generate go run ../../cmd/gen-meta --schema ../spec/geppetto_codegen.yaml --section turns-dts
+//go:generate go run ../../cmd/tools/gen-meta --schema ../spec/geppetto_codegen.yaml --section turns-go
+//go:generate go run ../../cmd/tools/gen-meta --schema ../spec/geppetto_codegen.yaml --section turns-dts
